331ba9f2954f16dfe529863e89ac48263c7e164e
Taryn Orlemann <106449394+torlemann@users.noreply.github.com>
2022-09-24 21:39:34 -0700

    Revert "PR for User story 1"

--- app/controllers/bulk_discounts_controller.rb
+++
@@ -1,9 +0,0 @@
-class BulkDiscountsController < ApplicationController
-    def index
-      @merchant = Merchant.find(params[:merchant_id])
-    end
-  
-    def show
-  
-    end
-  end 
--- app/models/bulk_discount.rb
+++
@@ -1,6 +0,0 @@
-class BulkDiscount < ApplicationRecord
-    validates :percentage_discount, presence: true
-    validates :quantity_threshold, presence: true
-    validates :merchant_id, presence: true
-    belongs_to :merchant
-end
