ac3ab8d8893baa6ed4100c2eaffaf73420c931a5
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-27 22:49:05 -0700

    Add discount used method to invoice and invoice view

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -16,4 +16,10 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def discount_used
+    bulk_discounts.where("#{quantity} >= bulk_discounts.quantity_threshold")
+    .order(percentage_discount: :desc)
+    .first
+  end 
 end
